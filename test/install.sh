9c981c2dc54486766fcff1502608aea8a90a6536
root <root@ilia-netbook.(none)>
2010-11-21 18:49:53 -0500

    final again

--- test/install.sh
+++ test/install.sh
@@ -8,3 +8,8 @@ curl http://npmjs.org/install.sh | sh
 npm install express jade redis socket.io yaml
 cd C_server
 make && gcc -o ./hiredis.o -c ./hiredis.c && g++ -O3 -Wall -o serverTest ./server3.cpp ./*.o
+sysctl -w net.ipv4.tcp_rmem="4096 4096 4096"
+sysctl -w net.ipv4.tcp_wmem="4096 4096 4096"
+sysctl -w net.core.wmem_max="8388608"
+sysctl -w net.core.rmem_max="8388608"
+ulimit -n 1048576
